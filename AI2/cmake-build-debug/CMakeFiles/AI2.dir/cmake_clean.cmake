88f94582fd279569526d4677eb41ca2a9506cd28
sk-31 <821991728@qq.com>
2018-07-14 18:50:25 +0800

    from linux

---
+++ AI2/cmake-build-debug/CMakeFiles/AI2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AI2.dir/main.cpp.o"
+  "AI2.pdb"
+  "AI2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AI2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ jing_zi/cmake-build-debug/CMakeFiles/jing_zi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jing_zi.dir/main.cpp.o"
+  "jing_zi.pdb"
+  "jing_zi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jing_zi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
